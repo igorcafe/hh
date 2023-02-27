f4fa720312f82489e5afe3ee814a5a7eeae86d7c
Igor Melo <igoracmelo@protonmail.com>
2023-02-27 20:24:19 -0300

    feat: hash string, flag parsing, usage

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"hash"
 	"hash/crc32"
@@ -13,15 +14,40 @@ import (
 	"os"
 )
 
+var sFlag string
+
+func init() {
+	flag.StringVar(&sFlag, "s", "", "hash a specific string instead of files")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s ALGORITHM FILE...\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s -s STRING ALGORITHM\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Println()
+
+		fmt.Fprintf(os.Stderr, "Examples:\n")
+		fmt.Fprintf(os.Stderr, "  %s sha256 /usr/bin/ls\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s -s 'string to be hashed' md5\n", os.Args[0])
+	}
+
+	flag.Parse()
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <algorithm> <file>\n", os.Args[0])
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if sFlag == "" && flag.NArg() < 2 {
+		flag.Usage()
 		os.Exit(1)
 	}
 
 	var h hash.Hash
 
-	switch os.Args[1] {
+	switch flag.Args()[0] {
 	case "md5":
 		h = md5.New()
 	case "sha1":
@@ -38,9 +64,14 @@ func main() {
 		h = crc32.NewIEEE()
 	}
 
-	fnames := os.Args[2:]
+	if sFlag != "" {
+		_, _ = h.Write([]byte(sFlag))
+		res := h.Sum(nil)
+		fmt.Printf("%s  %s\n", hex.EncodeToString(res), sFlag)
+		return
+	}
 
-	for _, fname := range fnames {
+	for _, fname := range flag.Args()[1:] {
 		s, err := computeFileHash(h, fname)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to compute hash for file %s: %s\n", fname, err.Error())
