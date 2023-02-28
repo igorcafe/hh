6a2a09eef6e00a315e529b23ef29247343a198ce
Igor Melo <igoracmelo@protonmail.com>
2023-02-27 23:44:21 -0300

    reduce CPU load with time.Sleep

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"runtime"
 	"sync/atomic"
+	"time"
 )
 
 var sFlag string
@@ -116,6 +117,7 @@ func main() {
 				if c == i {
 					break
 				}
+				time.Sleep(time.Millisecond)
 			}
 
 			// If hashing failed, show error on stderr, otherwise print hash & filenmae
