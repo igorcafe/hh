0bdccc5aac2bd50b20926a0bf90eb58689efd0d5
Igor Melo <igoracmelo@protonmail.com>
2023-02-27 21:39:24 -0300

    feat: compute hashes in parallel (opt out)

--- main.go
+++ main.go
@@ -12,17 +12,21 @@ import (
 	"hash/crc32"
 	"io"
 	"os"
+	"runtime"
+	"sync/atomic"
 )
 
 var sFlag string
+var noParallelFlag bool
 
 func init() {
 	flag.StringVar(&sFlag, "s", "", "hash a specific string instead of files")
+	flag.BoolVar(&noParallelFlag, "nop", false, "disable hash parallel computing")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "  %s ALGORITHM FILE...\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "  %s -s STRING ALGORITHM\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s [FLAG]... ALGORITHM FILE...\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s [FLAG]... -s STRING ALGORITHM\n", os.Args[0])
 		flag.PrintDefaults()
 		fmt.Println()
 
@@ -45,45 +49,90 @@ func main() {
 		os.Exit(1)
 	}
 
-	var h hash.Hash
+	var newHash func() hash.Hash
 
 	switch flag.Args()[0] {
 	case "md5":
-		h = md5.New()
+		newHash = md5.New
 	case "sha1":
-		h = sha1.New()
+		newHash = sha1.New
 	case "sha224":
-		h = sha256.New224()
+		newHash = sha256.New224
 	case "sha256":
-		h = sha256.New()
+		newHash = sha256.New
 	case "sha384":
-		h = sha512.New384()
+		newHash = sha512.New384
 	case "sha512":
-		h = sha512.New()
+		newHash = sha512.New
 	case "crc32":
-		h = crc32.NewIEEE()
+		newHash = func() hash.Hash {
+			return crc32.NewIEEE()
+		}
 	}
 
 	if sFlag != "" {
+		h := newHash()
 		_, _ = h.Write([]byte(sFlag))
 		res := h.Sum(nil)
 		fmt.Printf("%s  %s\n", hex.EncodeToString(res), sFlag)
 		return
 	}
 
-	for _, fname := range flag.Args()[1:] {
-		s, err := computeFileHash(h, fname)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to compute hash for file %s: %s\n", fname, err.Error())
-			continue
-		}
-
-		fmt.Printf("%s  %s\n", s, fname)
+	maxGoroutines := runtime.NumCPU()
+	if noParallelFlag {
+		maxGoroutines = 1
 	}
+
+	chLimit := make(chan struct{}, maxGoroutines)
+	curr := int32(0)
+	nfiles := flag.NArg() - 1
+	finished := make(chan struct{})
+
+	for i, fname := range flag.Args()[1:] {
+		i := int32(i)
+		h := newHash()
+		fname := fname
+		chLimit <- struct{}{}
+
+		go func() {
+			msg := ""
+			ok := true
+			s, err := computeFileHash(h, fname)
+
+			if err != nil {
+				ok = false
+				msg = fmt.Sprintln(err.Error())
+			} else {
+				msg = fmt.Sprintf("%s  %s\n", s, fname)
+			}
+
+			for {
+				c := atomic.LoadInt32(&curr)
+				if c == i {
+					break
+				}
+			}
+
+			if ok {
+				fmt.Print(msg)
+			} else {
+				fmt.Fprint(os.Stderr, msg)
+			}
+
+			if i == int32(nfiles)-1 {
+				close(finished)
+			} else {
+				atomic.StoreInt32(&curr, i+1)
+			}
+
+			<-chLimit
+		}()
+	}
+
+	<-finished
 }
 
 func computeFileHash(h hash.Hash, fname string) (string, error) {
-	defer h.Reset()
 	f, err := os.Open(fname)
 	if err != nil {
 		return "", err
